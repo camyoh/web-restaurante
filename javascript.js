7295a29d2597094512db47e51d84cbdb0995f5b5
camyoh <carlos.camyoh@gmail.com>
2018-06-04 21:22:59 -0500

    Cambia el dia de la semana

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 var fecha = new Date();
 var textoAbierto = document.getElementsByClassName('estado');
 var textoHorario = document.getElementsByClassName('tiempo-falta');
+var tituloDia = document.getElementsByClassName('tituloDia');
 
 function horario(fecha) {
     var hora = fecha.getHours();
@@ -20,19 +21,62 @@ function horario(fecha) {
         if (hora >= 15 && hora < 17)
             textoHorario[0].textContent = "Abre a las 5 P.M. hoy.";
     }
+    var dia = fecha.getDay();
+    var caja = document.getElementsByClassName("imgDelDia");
+    switch (dia) {
+        case 1:
+            dia = "Lunes";
+            caja[0].style.backgroundImage = 'url(images/lunes.jpg)';
+            break;
+        case 2:
+            dia = "Martes";
+            caja[0].style.backgroundImage = 'url(images/martes.jpg)';
+            break;
+        case 3:
+            dia = "Miércoles";
+            caja[0].style.backgroundImage = 'url(images/miercoles.jpg)';
+            break;
+        case 4:
+            dia = "Jueves";
+            caja[0].style.backgroundImage = 'url(images/jueves.jpg)';
+            break;
+        case 5:
+            dia = "Viernes";
+            caja[0].style.backgroundImage = 'url(images/viernes.jpg)';
+            break;
+        case 6:
+            dia = "Sábado";
+            caja[0].style.backgroundImage = 'url(images/sabado.jpg)';
+            break;
+        case 7:
+            dia = "Domingo";
+            caja[0].style.backgroundImage = 'url(images/domingo.jpg)';
+            break;
+        default:
+            break;
+    }
+    tituloDia[0].textContent = "Hoy es " + dia + " de ";
+    console.log(dia);
+
+
+
 }
 horario(fecha);
+
+
+
+
 
 //var caja2 = document.getElementsByClassName('cajagal2');
 //$(document).ready(function () {
 
-$(window).scroll(function(){
-   
+$(window).scroll(function () {
+
     var wScroll = $(this).scrollTop();
-    
-    $('.logo').css('transform', 'translate(0px,'+ wScroll/2 +'%)');
-    $('.detraslogo').css('transform', 'translate(0px,'+ wScroll/2.5 +'%)');
-    
+
+    $('.logo').css('transform', 'translate(0px,' + wScroll / 2 + '%)');
+    $('.detraslogo').css('transform', 'translate(0px,' + wScroll / 2.5 + '%)');
+
     console.log("hi");
 });
 
@@ -59,12 +103,12 @@ $(".vermasmenos").on("click", function () {
     $(".cajagal2").slideToggle();
 
     if (num == 0) {
-        $(".imagen").attr("src","images/Botonmenos.png");
-        num =1;
+        $(".imagen").attr("src", "images/Botonmenos.png");
+        num = 1;
 
     } else {
-        $(".imagen").attr("src","images/Botonmas.png");
-        num =0;
+        $(".imagen").attr("src", "images/Botonmas.png");
+        num = 0;
     }
 
 
